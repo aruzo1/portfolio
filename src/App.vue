ad0448a3cb3a018b41be8ccddac19357e32dc361
Aruzo <dawid12391111@gmail.com>
2022-06-27 01:20:26 +0200

    #7 created home section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import Navbar from "@/components/navbar/Navbar.vue";
+import Home from "./components/home/Home.vue";
 import "@/styles/style.scss";
 </script>
 
 <template>
   <div class="container">
     <Navbar />
+    <Home />
   </div>
 </template>
