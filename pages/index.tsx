6cf193fb21903880e2f3078bc62f171a443b5f8b
Aruzo <dawid12391111@gmail.com>
2022-06-29 11:35:53 +0200

    #17 initalized nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Navbar from "components/Navbar";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
