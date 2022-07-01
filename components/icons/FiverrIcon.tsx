544dd3ffdeee4b93d0539516cf11e7f8a5abc314
Aruzo <dawid12391111@gmail.com>
2022-07-01 23:59:14 +0200

    #32 created icons folder

---
+++ components/icons/FiverrIcon.tsx
@@ -0,0 +1,30 @@
+function FiverrIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+    >
+      <g transform="translate(6 10)">
+        <g transform="translate(-6 -10)">
+          <circle
+            id="Ellipse_1"
+            data-name="Ellipse 1"
+            cx="16"
+            cy="16"
+            r="16"
+            fill="currentColor"
+          />
+        </g>
+        <path
+          d="M157.61,143.875h-7.762v-.509c0-1,1-1.018,1.508-1.018a4.557,4.557,0,0,1,.848.057v-2.751a9.8,9.8,0,0,0-1.244-.075c-1.621,0-4.617.452-4.617,3.882v.436H144.93v2.544h1.414v5.37h-1.319v2.544h6.368v-2.546h-1.545v-5.37h4.238v5.37h-1.317v2.544h6.162v-2.544h-1.32Z"
+          transform="translate(-141.93 -140.967)"
+          fill="#fff"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default FiverrIcon;
---
+++ components/icons/InstagramIcon.tsx
@@ -0,0 +1,41 @@
+function InstagramIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+    >
+      <g transform="translate(-0.301 -0.301)">
+        <circle
+          cx="16"
+          cy="16"
+          r="16"
+          transform="translate(0.301 0.301)"
+          fill="currentColor"
+        />
+      </g>
+      <g id="Group_15" data-name="Group 15" transform="translate(5.812 5.812)">
+        <g>
+          <path
+            d="M38.857,44.376H29.519A5.525,5.525,0,0,1,24,38.857V29.519A5.525,5.525,0,0,1,29.519,24h9.339a5.525,5.525,0,0,1,5.519,5.519v9.339A5.525,5.525,0,0,1,38.857,44.376ZM29.519,25.7a3.825,3.825,0,0,0-3.82,3.82v9.339a3.825,3.825,0,0,0,3.82,3.82h9.339a3.825,3.825,0,0,0,3.82-3.82V29.519a3.825,3.825,0,0,0-3.82-3.82Z"
+            transform="translate(-24 -24)"
+            fill="#fff"
+          />
+        </g>
+        <g transform="translate(5.094 5.094)">
+          <path
+            d="M49.094,54.188a5.094,5.094,0,1,1,5.094-5.094A5.1,5.1,0,0,1,49.094,54.188Zm0-8.49a3.4,3.4,0,1,0,3.4,3.4A3.4,3.4,0,0,0,49.094,45.7Z"
+            transform="translate(-44 -44)"
+            fill="#fff"
+          />
+        </g>
+        <g transform="translate(14.433 3.608)">
+          <circle cx="1.167" cy="1.167" r="1.167" fill="#fff" />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default InstagramIcon;
