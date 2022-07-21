215909e32364e38614ea9f78fc0c024e7e9b0173
Aruzo <dawid12391111@gmail.com>
2022-07-21 15:36:09 +0200

    #39 added projects

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -9,9 +9,30 @@ const projects: IProject[] = [
   {
     img: "/images/cosmac.png",
     title: "Cosmac",
-    desc: "Website created for nail salon. Main features: \n-Modal showing product details \n-Products organized by categories \n-Products searching bar",
+    desc: "Website created for nail salon.\n- Modal showing product details \n- Products organized by categories \n- Products searching bar",
+    href: "https://cosmac.vercel.app/",
     links: [{ Icon: GithubIcon, href: "https://github.com/aruzo1/cosmac" }],
     badges: ["Next.js", "Django Rest Framework", "TypeScript", "Tailwind"],
+  },
+  {
+    img: "/images/sincity.png",
+    title: "Sincity Banditz",
+    desc: "Website created for esport team SinCity Banditz.\n- Welcome section \n- Team section \n- Form sending emails",
+    href: "https://esport-website.vercel.app/",
+    links: [
+      { Icon: GithubIcon, href: "https://github.com/aruzo1/sincity-banditz" },
+    ],
+    badges: ["Next.js", "TypeScript", "Tailwind"],
+  },
+  {
+    img: "/images/pokedex.png",
+    title: "Pokedex",
+    desc: "Website created for Pokémon fans.\n- Infinity scroll \n- Sorting filters",
+    href: "https://pokedex-aruzo1.vercel.app/",
+    links: [
+      { Icon: GithubIcon, href: "https://github.com/aruzo1/pokemon-app" },
+    ],
+    badges: ["Next.js", "GraphQL", "PokeAPI", "TypeScript", "Tailwind"],
   },
 ];
 
@@ -23,8 +44,8 @@ function Projects() {
           <Highlight>My</Highlight> Projects
         </SectionTitle>
         <ProjectsList>
-          {projects.map((project) => (
-            <Project key={project.title} {...project} />
+          {projects.map((project, i) => (
+            <Project key={project.title} {...project} right={i % 2 !== 0} />
           ))}
         </ProjectsList>
       </ProjectsStyled>
--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,8 @@ export interface IProject {
   img: string;
   title: string;
   desc: string;
+  href: string;
   links: { Icon: ComponentType; href: string }[];
   badges: string[];
+  right?: boolean;
 }
